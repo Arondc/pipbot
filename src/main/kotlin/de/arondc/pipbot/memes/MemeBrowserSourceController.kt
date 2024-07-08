cd80d305ab7e0efa66a2b9ad0dd33d92e6ef27e3
Sten Seifert <sten.seifert@googlemail.com>
2024-07-08 15:29:04 +0200

    feat: Update Meme browser source feature
    * add gif support
    * add meme monitor that shows last shown meme via an own URL
    * made obs browser source configurable via optional url parameters
    * moved img link logic to backend
    * updated documentation for meme overlay feature

--- src/main/kotlin/de/arondc/pipbot/memes/MemeBrowserSourceController.kt
+++ src/main/kotlin/de/arondc/pipbot/memes/MemeBrowserSourceController.kt
@@ -3,24 +3,41 @@ package de.arondc.pipbot.memes
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("/browser/memes")
 class MemeBrowserSourceController(val memeBrowserSourceQueue: MemeBrowserSourceQueue) {
 	@GetMapping("/{channelName}")
-	fun source(@PathVariable channelName: String, model: Model): String {
+	fun obsBrowserSource(
+		@PathVariable channelName: String,
+		@RequestParam imgWidth: String = "600px",
+		@RequestParam address: String = "localhost:8080",
+		model: Model
+	): String {
 		model.set("channelName", channelName)
-		return "memesource"
+		model.set("imgWidth", imgWidth)
+		model.set("address", address)
+		return "memeobssource"
 	}
 
-	@GetMapping("/{channelName}/message")
+	@GetMapping("/{channelName}/monitor")
+	fun monitorBrowserSource(@PathVariable channelName: String, model: Model): String {
+		model.set("channelName", channelName)
+		return "mememonitorsource"
+	}
+
+	@GetMapping("/{channelName}/next_meme")
 	@ResponseBody
-	fun message(@PathVariable channelName: String): String {
+	fun nextMeme(@PathVariable channelName: String): String {
 		val nextMeme = memeBrowserSourceQueue.getNextMeme(channelName)
 		return nextMeme ?: ""
+	}
+
+	@GetMapping("/{channelName}/last_returned_meme")
+	@ResponseBody
+	fun lastReturnedMeme(@PathVariable channelName: String): String {
+		val lastReturnedMeme = memeBrowserSourceQueue.getLastReturnedMeme(channelName)
+		return lastReturnedMeme ?: ""
 	}
 }
--- src/main/kotlin/de/arondc/pipbot/memes/MemeBrowserSourceQueue.kt
+++ src/main/kotlin/de/arondc/pipbot/memes/MemeBrowserSourceQueue.kt
@@ -1,14 +1,46 @@
 package de.arondc.pipbot.memes
 
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 
 @Component
 class MemeBrowserSourceQueue {
+    private val log = KotlinLogging.logger {}
+
     val memesForChannel = mutableMapOf<String, ArrayDeque<String>>()
+    val lastReturnedMemeForChannel = mutableMapOf<String, String>()
 
     fun queue(channelName: String, meme : String){
-        memesForChannel.compute(channelName){ _, list ->
-            when(list) {
+        val memeLink = convertToImageLink(meme)
+        if (memeLink != null) {
+            addToQueue(channelName, memeLink)
+        }
+    }
+
+    fun getNextMeme(channelName: String): String? {
+        val nextMeme = memesForChannel[channelName]?.removeFirstOrNull()
+        if (nextMeme != null) {
+            lastReturnedMemeForChannel[channelName] = nextMeme
+        }
+        return nextMeme
+    }
+
+    fun getLastReturnedMeme(channelName: String): String? = lastReturnedMemeForChannel[channelName]
+
+    fun convertToImageLink(meme : String) : String? {
+        return when {
+            meme.contains("imgflip.com/i/") -> meme.replace("imgflip.com/i","i.imgflip.com") + ".jpg"
+            meme.contains("imgflip.com/gif/") -> meme.replace("imgflip.com/gif","i.imgflip.com") + ".gif"
+            else -> {
+                log.warn { "Could not process the imgflip link '$meme' for meme overlay" }
+                null
+            }
+        }
+    }
+
+    private fun addToQueue(channelName: String, meme: String) {
+        memesForChannel.compute(channelName) { _, list ->
+            when (list) {
                 null -> {
                     val newList = ArrayDeque<String>()
                     newList.add(meme)
@@ -18,8 +50,8 @@ class MemeBrowserSourceQueue {
                     list.addLast(meme)
                     list
                 }
-            }}
+            }
+        }
     }
 
-    fun getNextMeme(channelName: String): String? = memesForChannel[channelName]?.removeFirstOrNull()
 }
